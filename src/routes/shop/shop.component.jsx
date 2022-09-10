65fb5e0ddef8fe6a1f6e84729331dc73a9881cc3
Prabhjot <sandhu7707@gmail.com>
2022-09-10 21:33:58 +0530

    homepage

---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,3 @@
+const Shop = () => <h1>bellooo! it's a shop!</h1>
+
+export default Shop;
---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,9 @@
+const SignIn = () => {
+    return(
+        <div>
+            SignIn Page
+        </div>
+    )
+}
+
+export default SignIn
